53e275d9cb2c70daadabe7b592f1bc11abdb81d3
codefaux <codefaux@solacenet.org>
2026-02-01 13:39:10 -0800

    Add local test for basic debugging, vscode settings etc

---
+++ fauxlogger/dev_test.py
@@ -0,0 +1,10 @@
+import logger as _log
+
+_log.msg("testing\ntest")
+_log.msg("testing1")
+_log.msg("testing2\ntest\ntest")
+_log.msg("testing3\ntest\ntest\ntest")
+_log.msg("testing3\ntest\ntest\ntest\ntest\ntest")
+_log.msg("testing3\ntest\ntest\ntest")
+
+pass
